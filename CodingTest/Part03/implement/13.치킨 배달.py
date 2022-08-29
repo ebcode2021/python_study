39100f49307004d86bcd486324139306b4547841
ebcode2021 <ebcode2021@gmail.com>
2022-08-29 17:23:58 +0900

    middle

--- CodingTest/Part03/implement/13.치킨 배달.py
+++ CodingTest/Part03/implement/13.치킨 배달.py
@@ -1,5 +1,24 @@
-size, goal_chicken_cnt = map(int, input().split())
-map = [[0] * (size) for _ in range(size)]
+from itertools import product, permutations, combinations
 
+def chicken_distance(return_chicken, map) :
+	for _ in range(return_chicken) :
+		
+	return (distance)
 
+size, chicken_shop_cnt = map(int, input().split())
+my_map = []
+chicken_location = []
 
+for i in range(size) :
+	my_map.append(list(map(int, input().split())))
+	while my_map[i].index(2) != -1 :
+		chicken_location.append([i, my_map[i].index(2)])
+
+return_chickens = list(permutations(chicken_location, chicken_shop_cnt))
+min_location = 99999
+
+for return_chicken in len(return_chickens) :
+	if min_location > chicken_distance(return_chicken, my_map) :
+		min_location = chicken_distance(return_chicken, my_map)
+
+print(min_location)
